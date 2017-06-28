cf944217b17ee099c5c026538ba6d3f469a41d3b
Lucija M <lucka318@users.noreply.github.com>
2017-06-28 16:05:34 +0200

    ignore

--- polisher.py
+++ polisher.py
@@ -12,20 +12,20 @@ def main(arguments):
 
 	parser = argparse.ArgumentParser(description=__doc__)
 
-	parser.add_argument("-r1", "--reference1",
-		required="True",
-		dest="ref1",
-		help="File containign reference, fasta format.")
+	# parser.add_argument("-r1", "--reference1",
+	# 	required="True",
+	# 	dest="ref1",
+	# 	help="File containign reference, fasta format.")
 
 	# parser.add_argument("-r2", "--reference2",
 	# 	required="True",
 	# 	dest="ref2",
 	# 	help="File containign reference, fasta format.")
 
-	parser.add_argument("-s1", "--reads1",
-    	required="True",
-    	dest="reads1",
-    	help="Reads aligned to the reference. SAM format.")
+	# parser.add_argument("-s1", "--reads1",
+ #    	required="True",
+ #    	dest="reads1",
+ #    	help="Reads aligned to the reference. SAM format.")
 
 	# parser.add_argument("-s2", "--reads2",
  #    	required="True",
@@ -44,14 +44,14 @@ def main(arguments):
 
 	args = parser.parse_args(arguments)
 
-	read_max_1, regions_dict_1 = re.readerrors(args.ref1, args.reads1)
+	#read_max_1, regions_dict_1 = re.readerrors(args.ref1, args.reads1)
 
 	#read_max_2, regions_dict_2 = re.readerrors(args.ref2, args.reads2)
 
 	read_max_c, regions_dict_c = re.readerrors(args.cons, args.cons_reads)
 
-	read_max = max(read_max_1,read_max_c)
-	re.make_test_csv(regions_dict_1, read_max, "train.csv")
+	read_max = max(7,read_max_c)
+	#re.make_test_csv(regions_dict_1, read_max, "train.csv")
 	#re.make_test_csv(regions_dict_2, read_max, "test.csv")
 	re.make_test_csv(regions_dict_c, read_max, "predict.csv")
 
